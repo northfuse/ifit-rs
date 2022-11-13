6e05e71506c85734456f2ea9d457d2286e876d58
Jim Bennett <jim.bennett@microsoft.com>
2022-11-12 18:10:56 -0800

    Added pagination to get all workout summaries

--- src/client.rs
+++ src/client.rs
@@ -45,8 +45,9 @@ impl IfitClient {
         Ok(())
     }
 
-    pub async fn list_workout_summaries(&self) -> Result<Vec<WorkoutSummary>> {
-        let raw_workouts = self.client.get("https://www.ifit.com/me/workouts")
+    async fn get_workout_summaries_for_page(&self, page: u8) -> Result<Vec<WorkoutSummary>> {
+        let url = format!("https://www.ifit.com/me/workouts?page={}", page);
+        let raw_workouts = self.client.get(url)
             .send()
             .await
             .context("unable to load workouts")?
@@ -55,6 +56,22 @@ impl IfitClient {
             .context("unable to load workout response body")?;
 
         let workouts = parse_workout_summaries(raw_workouts)?;
+        Ok(workouts)
+    }
+
+    pub async fn list_workout_summaries(&self) -> Result<Vec<WorkoutSummary>> {
+        let mut workouts = Vec::new();
+
+        let mut page = 1;
+        loop {
+            let page_workouts = self.get_workout_summaries_for_page(page).await?;
+            if page_workouts.len() == 0 {
+                break;
+            }
+            workouts.extend(page_workouts);
+            page += 1;
+        }
+
         Ok(workouts)
     }
 
